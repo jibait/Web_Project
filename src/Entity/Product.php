11e799f1535ea5ce19234969aec86e714e1bc025
Jean-Baptiste <jb.lipo562@gmail.com>
2023-06-20 07:36:56 +0200

    first commit

---
+++ src/Entity/Product.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ProductRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ProductRepository::class)]
+class Product
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $product_title = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $product_description = null;
+
+    #[ORM\Column]
+    private ?int $price = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $product_img = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $product_rate = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $product_tag = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getProductTitle(): ?string
+    {
+        return $this->product_title;
+    }
+
+    public function setProductTitle(string $product_title): static
+    {
+        $this->product_title = $product_title;
+
+        return $this;
+    }
+
+    public function getProductDescription(): ?string
+    {
+        return $this->product_description;
+    }
+
+    public function setProductDescription(?string $product_description): static
+    {
+        $this->product_description = $product_description;
+
+        return $this;
+    }
+
+    public function getPrice(): ?int
+    {
+        return $this->price;
+    }
+
+    public function setPrice(int $price): static
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    public function getProductImg(): ?string
+    {
+        return $this->product_img;
+    }
+
+    public function setProductImg(string $product_img): static
+    {
+        $this->product_img = $product_img;
+
+        return $this;
+    }
+
+    public function getProductRate(): ?string
+    {
+        return $this->product_rate;
+    }
+
+    public function setProductRate(?string $product_rate): static
+    {
+        $this->product_rate = $product_rate;
+
+        return $this;
+    }
+
+    public function getProductTag(): ?string
+    {
+        return $this->product_tag;
+    }
+
+    public function setProductTag(string $product_tag): static
+    {
+        $this->product_tag = $product_tag;
+
+        return $this;
+    }
+}
